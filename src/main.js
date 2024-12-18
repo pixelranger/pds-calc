82d10f71bcb730ffedd3dded7e12d63a06bf7add
Nikolai Dolgushin <nikolay.klnt@gmail.com>
2024-12-18 16:10:12 +0400

    Rename widget

--- src/main.js
+++ src/main.js
@@ -1,22 +1,18 @@
 // Import the bootstrap function from the 'bootstrap.js' file
 import { bootstrap } from './bootstrap.js';
 
-// Define a custom element 'countdown-widget' using the Custom Elements API
-customElements.define('quiz-widget', class extends HTMLElement {
+// Define a custom element 'pds-calculator' using the Custom Elements API
+customElements.define('pds-calculator', class extends HTMLElement {
 
   async connectedCallback() {
 
-    // Create a shadow DOM for encapsulatio
+    // Create a shadow DOM for encapsulation
     const shadowRoot = this.attachShadow({ mode: 'open' });
 
-    // Fetch <countdown-widget> attributes
+    // Fetch <pds-calculator> attributes
     // to pass as properties for App.vue
 
     const attributes = {
-      'firstname': this.getAttribute('firstname'),
-      'lastname': this.getAttribute('lastname'),
-      'email': this.getAttribute('email'),
-      'settings': this.getAttribute('settings'),
       'apiUrl': this.getAttribute('apiUrl'),
     }
 
