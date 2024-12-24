5a81891628b9460375e3eebfc2de5e8b4ae3e9f7
sttshka <sttshka.dev@gmail.com>
2024-12-24 14:48:54 +0300

    Rebuild fixes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <!--Link to compiled style.css-->
-  <link rel="stylesheet" href="/quiz_widgets/variant-1/style.css">
+  <Suspense>
+    <Main :api-url="apiUrl"/>
+  </Suspense>
 
-  <Main
-    :api-url="apiUrl"
-  />
+  <!--  Encapsulating all css rules for shadow-dom-->
+  <component :is="'style'">{{styles}}</component>
 </template>
+
 
 
 <script setup>
 import { defineProps } from 'vue';
-import Main from "@/components/Main.vue";
+import styles from '../pds-calculator/widget.css?inline';
+import Main from '@/components/Main.vue';
+
 defineProps({
   apiUrl: {
     type: String,
